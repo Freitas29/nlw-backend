302590ec79d74015f8df31a1313d2d072df53eb7
Freitas29 <arao.freitas@hotmail.com>
2020-06-03 10:20:09 -0300

    testing item controller

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import express from 'express'
-import routes from './routes'
-import path from 'path'
-import cors from 'cors'
+import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-app.use(cors())
+app.use(cors());
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3000)
+app.listen(3000);
+
+export default app;
---
+++ test/Item.spec.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+import app from "../src/server";
+
+describe('Testing Item controller', () => {
+  it("Should create a item with success", async () => {
+    const res = await request(app).get("/items");
+  
+    expect(res.status).toBe(200);
+    
+    const item = res.body[0]
+    
+    expect(item).toHaveProperty('title')
+  });
+  
+})
