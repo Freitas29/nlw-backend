d77c5b8a960bc57d6b19d6fb3d2954604c2dee0a
Freitas29 <arao.freitas@hotmail.com>
2020-06-02 23:45:46 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import routes from './routes'
 import path from 'path'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use(cors())
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
